7907464443c44e07f17e185fefa27c2f19ad1abf
Tamim Jabr <tj222kg@student.lnu.se>
2022-01-29 12:11:25 +0100

    refactor: move generating tokens to jwt-generator

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,5 +1,5 @@
 import createError from 'http-errors';
-import { generateJWT, Payload, decodeJWT } from './../helpers/jwt-generator';
+import { generateJWT, Payload, decodeJWT, generateAccessRefreshTokens } from './../helpers/jwt-generator';
 import { addUser, authorizeUser, getUserById } from './../respository/auth-respository';
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express'
@@ -8,7 +8,7 @@ import ValidationError from '../errors/ValidationError';
 import InvalidTokenError from '../errors/InvalidToken';
 
 
-const { ACCESS_TOKEN_SECRET, ACCESS_TOKEN_LIFE, REFRESH_TOKEN_SECRET, REFRESH_TOKEN_LIFE } = process.env
+const { REFRESH_TOKEN_SECRET } = process.env
 
 export class AuthController {
 
@@ -25,7 +25,6 @@ export class AuthController {
     } catch (error: any) {
       let err = error
       if (err.code === 11000) {
-        // Duplicated keys.
         err = new ConflictError('Email')
       } else if (error.name === 'ValidationError') {
         err = new ValidationError(Object.values(error.errors)
@@ -42,7 +41,8 @@ export class AuthController {
     try {
       const { email, password } = req.body
       const user = await authorizeUser(email, password)
-      const tokens = this._generateAccessRefreshTokens(user._id)
+      const tokens = generateAccessRefreshTokens(user._id)
+      
       res.status(200).json(tokens)
     } catch (error: any) {
       let err = error
@@ -61,16 +61,12 @@ export class AuthController {
         next(new InvalidTokenError('refresh'))
         return
       }
-      const tokens = this._generateAccessRefreshTokens(decoded?.userID)
+
+      const tokens = generateAccessRefreshTokens(decoded?.userID)
       res.status(201).json(tokens)
     } catch (error) {
       next(createError(401))
     }
   }
 
-  _generateAccessRefreshTokens (userID: string) {
-    const access_token = generateJWT({ userID }, ACCESS_TOKEN_SECRET!, ACCESS_TOKEN_LIFE!)
-    const refresh_token = generateJWT({ userID }, REFRESH_TOKEN_SECRET!, REFRESH_TOKEN_LIFE!)
-    return { access_token, refresh_token }
-  }
 }
--- src/helpers/jwt-generator.ts
+++ src/helpers/jwt-generator.ts
@@ -11,3 +11,10 @@ export const generateJWT = (payload: Payload, secret: string, life: string): str
 export const decodeJWT = (token: string): Payload => {
   return jwt.decode(token) as Payload;
 }
+
+export const generateAccessRefreshTokens = (userID: string): { access_token: string, refresh_token: string } => {
+  const { ACCESS_TOKEN_SECRET, ACCESS_TOKEN_LIFE, REFRESH_TOKEN_SECRET, REFRESH_TOKEN_LIFE } = process.env
+  const access_token = generateJWT({ userID }, ACCESS_TOKEN_SECRET!, ACCESS_TOKEN_LIFE!)
+  const refresh_token = generateJWT({ userID }, REFRESH_TOKEN_SECRET!, REFRESH_TOKEN_LIFE!)
+  return { access_token, refresh_token }
+}
