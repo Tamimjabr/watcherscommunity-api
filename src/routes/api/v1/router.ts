6212e898d02cbb49ed4688571e925b3d9066b960
Tamim Jabr <tj222kg@student.lnu.se>
2022-02-07 13:31:00 +0100

    add route to register webhook

--- src/routes/api/v1/router.ts
+++ src/routes/api/v1/router.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { router as authRouter } from './auth-router'
 import { router as userRouter } from './profile-router'
+import { router as webhookRouter } from './webhook-router'
 
 export const router = express.Router()
 
 router.use('/auth', authRouter)
 router.use('/users/:id/profile', userRouter)
+router.use('/webhooks', webhookRouter)
