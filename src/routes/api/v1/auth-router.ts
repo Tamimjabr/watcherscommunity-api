80ef30a24a683fa7d3df3f36320548363adf69c2
Tamim Jabr <tj222kg@student.lnu.se>
2022-02-10 17:56:52 +0100

    add API documentation

--- src/routes/api/v1/auth-router.ts
+++ src/routes/api/v1/auth-router.ts
@@ -4,10 +4,6 @@ import { AuthController } from '../../../controllers/auth-controller'
 export const router = express.Router()
 const controller = new AuthController()
 
-router.get('/', (req, res) => {
-  res.send('Auth route')
-})
-
 router.post('/register', (req, res, next) => {
   controller.register(req, res, next)
 })
