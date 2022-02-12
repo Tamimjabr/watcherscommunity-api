b3948a2824b2c64a9e69d4cd3a5819326ba842d7
Tamim Jabr <tj222kg@student.lnu.se>
2022-02-12 13:39:51 +0100

    insert test data into DB using seeder script

---
+++ src/data/test-data.ts
@@ -0,0 +1,54 @@
+import bycrpt from 'bcrypt';
+
+const users = [
+  {
+    email: 'exampleuser@gmail.com',
+    password: bycrpt.hashSync('password44sd5ds5s', 10),
+  },
+  {
+    email: 'exampleuser2@gmail.com',
+    password: bycrpt.hashSync('pasqwqw!!sword44sd5ds5s', 10),
+  },
+  {
+    email: 'exampleuser3@gmail.com',
+    password: bycrpt.hashSync('pa123sqwqw!!sw12?+_3ord44sd5ds5s', 10),
+  }
+]
+
+const profiles = [
+  {
+    wallets: [
+      "QmWLxGtt7xkKZ7XJq7S6Xf7BhjmXf7pQQmvqJQm7qfN",
+      "ZJXEQ6BPRPKZFC6DZBLPQTINTIB3TZLCUDIA4",
+      "asdfwerew7pQQmvqJQm7ewrwerwerwerqfN",
+    ],
+    preferredCurrency: 'usd',
+  }, {
+    wallets: [
+      "Jq7S6Xf7BhjmXf7pQQmvqJQm7qfN",
+      "KZFC6DZBLPQTINTIB3TZLCUDIA4",
+      "a7pQQmvqJQm7ewrwerwerwerqfN",
+    ],
+    preferredCurrency: 'sek'
+  },
+  {
+    wallets: [],
+  }
+]
+
+const webhooks = [
+  {
+    webhooks:
+      [{
+        url: 'https://example.com/webhook1',
+        event: 'LoginEvent',
+        secret: 'secret1558sad9'
+      }]
+  }
+]
+
+
+
+export default {
+  users, profiles, webhooks
+}
--- src/seeders/seeder.ts
+++ src/seeders/seeder.ts
@@ -3,16 +3,35 @@ import { UserModel } from "../models/user"
 import { ProfileModel } from "../models/profile"
 import { UserWebhookModel } from "../models/webhook"
 import { TokenModel } from '../models/token';
+import TESTDATA from '../data/test-data'
 
 const seedDB = async () => {
   await connectDB()
+
   await UserModel.deleteMany()
   await TokenModel.deleteMany()
   await ProfileModel.deleteMany()
   await UserWebhookModel.deleteMany()
 
+  const savedUsers = await UserModel.insertMany(TESTDATA.users)
+
+  const profilesToInsert = TESTDATA.profiles.map((profile, index) => {
+    return { ...profile, userID: savedUsers[index]._id }
+  })
+  await ProfileModel.insertMany(profilesToInsert)
+
+  const webhooksToInsert = TESTDATA.webhooks.map((webhook, index) => {
+    return { ...webhook, userID: savedUsers[index]._id }
+  })
+  await UserWebhookModel.insertMany(webhooksToInsert)
+
   console.log('Data has been seeded successfully')
   process.exit()
 }
 
-seedDB().catch(console.error)
+try {
+  seedDB()
+} catch (error) {
+  console.log(error)
+  process.exit(1)
+}
