b84b97be66eaf9bf7fde4f98df1d9f70aae8be8f
Tamim Jabr <tj222kg@student.lnu.se>
2022-02-12 12:20:41 +0100

    insert objects into links array

--- src/helpers/links-creator.ts
+++ src/helpers/links-creator.ts
@@ -5,6 +5,7 @@ export const createAuthLinks = (req: Request) => {
   const controllerName = req.path.split('/').pop()!
   const links = [
     {
+      name: 'register',
       URL: `${req.protocol}://${req.get('host')}${req.originalUrl.replace(controllerName, 'register')}`,
       method: 'POST',
       description: 'Register a new user',
@@ -14,6 +15,7 @@ export const createAuthLinks = (req: Request) => {
       }
     },
     {
+      name: 'login',
       URL: `${req.protocol}://${req.get('host')}${req.originalUrl.replace(controllerName, 'login')}`,
       method: 'POST',
       description: 'Login a user',
@@ -23,6 +25,7 @@ export const createAuthLinks = (req: Request) => {
       }
     },
     {
+      name: 'refresh',
       URL: `${req.protocol}://${req.get('host')}${req.originalUrl.replace(controllerName, 'refresh')}`,
       method: 'POST',
       description: 'Refresh a user token',
@@ -31,6 +34,7 @@ export const createAuthLinks = (req: Request) => {
       }
     },
     {
+      name: 'logout',
       URL: `${req.protocol}://${req.get('host')}${req.originalUrl.replace(controllerName, 'logout')}`,
       method: 'DELETE',
       description: 'Logout a user',
@@ -41,5 +45,10 @@ export const createAuthLinks = (req: Request) => {
     }
   ]
 
-  return links
+  return {
+    register: links.find(link => link.name === 'register'),
+    login: links.find(link => link.name === 'login'),
+    refresh: links.find(link => link.name === 'refresh'),
+    logout: links.find(link => link.name === 'logout')
+  }
 }
